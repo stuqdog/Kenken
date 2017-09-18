c6c9492969880746cdabe4a9f35fe4b047ca6974
Ethan <ethanrodkin@gmail.com>
2017-09-18 10:11:01 -0400

    Moved find_possible method into cell class, added cluster_size variable.

--- Kenken/kenken.py
+++ Kenken/kenken.py
@@ -1,11 +1,19 @@
+puzzle_size = 3
+
 class Cell(object):
 
-    def __init__(self, x, y, formula, possible, actual):
+    def __init__(self, x, y, formula, possible, actual, cluster_size):
         self.x = x
         self.y = y
-        self.formula = formula
+        self.formula = formula #should be (operator, value) - or maybe just two vars.
         self.possible = possible
         self.actual = actual
+        self.cluster_size = cluster_size
+
+    def find_possible(cell):
+        if cell.formula[0] = "+":
+            for x in xrange(1, puzzle_size + 1):
+
 
 
 class CellCluster(object):
