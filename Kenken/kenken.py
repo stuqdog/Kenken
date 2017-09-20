4c260e826dea0b6c465b1cefff33c6f5545efb85
Ethan <ethanrodkin@gmail.com>
2017-09-19 22:57:46 -0400

    Set_possible_ints _should_ set individual possible ints for cells.

--- Kenken/kenken.py
+++ Kenken/kenken.py
@@ -11,6 +11,24 @@ class Cell(object):
         self.cluster_size = cluster_size
         self.possible_ints = []
 
+
+
+
+class CellCluster(object):
+
+    def __init__(self, cells, formula, cluster_size, possible): ## do we need possible and actual here?
+        self.cells = cells
+        self.formula = formula
+        self.cluster_size = cluster_size
+        self.possible = possible
+
+    def set_possible_ints(self):
+        for cell in self.cells:
+            for combo in self.possible:
+                for i in combo:
+                    if i not in cell.possible_ints:
+                        cell.possible_ints.append(i)
+
     def find_addition_values(self):
         test_values = [1] * self.cluster_size
             # Iterate up one at a time, starting with top values, until we reach
@@ -32,20 +50,14 @@ class Cell(object):
                 test_values[-x] -= 1
                 test_values[-x - 1] += 1
                 self.possible.append(test_values[:])
+        self.set_possible_ints()
 
     def find_subtraction_values(self):
         for x in range(self.formula[1] + 1, puzzle_size + 1):
             self.possible.append([x - self.formula[1], x])
+        self.set_possible_ints()
 
     def find_division_values(self):
         for x in range(1, puzzle_size / self.formula[1] + 1):
             self.possible.append([x, x * self.formula[1]])
-
-
-class CellCluster(object):
-
-    def __init__(self, cells, formula, cluster_size, possible): ## do we need possible and actual here?
-        self.cells = cells
-        self.formula = formula
-        self.cluster_size = cluster_size
-        self.possible = possible
+        self.set_possible_ints()
