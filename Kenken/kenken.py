3cea51675279d8ce39e6ab5a151d259530bf688b
Ethan <ethanrodkin@gmail.com>
2017-10-21 13:39:59 -0400

    + test works. * test I _think_ will work, need to test more to confirm.

--- Kenken/kenken.py
+++ Kenken/kenken.py
@@ -32,72 +32,16 @@ class CellCluster(object):
 
     def find_addition_values(self):
         arr = list(range(1, puzzles_size + 1)) * self.cluster_size
-        possible = list(set(x for x in itertools.combinations(
+        possible_long = list(set(x for x in itertools.combinations(
                arr, self.cluster_size) if sum(x) == self.formula[1]))
-        possible = [sorted(x) for x in possible]
-        short_possible = []
-        for x in possible:
-            if x not in short_possible:
-                short_possible.append(x)
-            # Iterate up one at a time, starting with top values, until we reach
-            # the sum we're looking for.
-
-        # Once we have reached sum, we lower the top value and raise lower
-        # values to find other possible combinations that reach the sum.
-
-        #But to solve the problem discussed above, we need to repeat this
-        #process over and over until all values are within one of the values
-        #next to them.
-
-        #Well, this doesn't quite solve it either. We're definitely still missing
-        #combinations.
-"""After doing this by hand, it seems like a possible method may be:
-1. set the [-1] entry at puzzle size, set everything else at 1
-2. if we're over sum, iterate down top value until we reach sum (if this happens,
-then we have our solution immediately)
-3. Then, set [-2] to whatever it needs to be to reach sum (or to [-1] val if
-that's not high enough to reach the sum)
-
-
-1. Okay so find the solution that best prioritized low values early (in the case
-of 4 +17, that would be 1/1/7/8.). Add that to the possible solutions.
-2. Then, for entry [-2], add one, and subtract one from [-1]. If it's not in possible
-   solutions (in this case it's 1/1/8/7 which is in possible_solutions), then we add that
-   to possible solutions. Then we add 2, then 3, etc. etc. until [-2] == puzzle size
-   (so in this case, we only do it once).
-3. Then we repeat the process, but start at [-3]. Add one, subtract one from [-1] and then
-   [-2], and each time add the result to possible_solutions if it's not there already, and then
-   do that with +/- 2, then 3, etc. until [-3] equal puzzle size.
-      The order ([-1] first, then [-2]) is important. Once an entry reaches value [0],
-      we know the math is going too far. But we don't want to miss anything. So start with
-      the higher numbers. <-- This is wrong. See n.b. below.
-4. then we go back to [-4] (not in this case, but you get the idea) and keep going back until
-   we reach an entry where its value is the same as [0]. Once we reach that point, we do the whole
-   +/- loop one more time, but don't bother going back to the previous index.
-5. At this point, we've done everything where smallest value is 1. So now we repeat the
-   whole process, but say 1 isn't allowed (so prioritize low early values, where 2 is the
-   lowest value and 1 is not allowed).
-6. Then we repeat that whole process until 3 * whatever our starting value is is >= sum.
-
-N.b.: This doesn't work at all. If we have 1, 2, 7, 7, it isn't capturing 1, 4, 6, 6.
-    OR MAYBE TI DOES??? Just not at the ones level.
-
-n.b.b.: After running through this by hand, it missed 3 4 5 5. Seemed to capture everything
-    else for the (4, +17) cluster. So, getting closer, but this still isn't it. Time to
-    try to come up with something better.
-"""
-
-        while True:
-            for x in range(1, self.cluster_size):
-                print test_values
-                while abs(test_values[-x] - test_values[-x - 1]) > 1:
-                    test_values[-x] -= 1
-                    test_values[-x - 1] += 1
-                    self.possible.append(test_values[:])
-            if all(abs(test_values[x] - test_values[x + 1]) <= 1
-                   for x in range(self.cluster_size - 1)):
-                break
+        possible_long = [sorted(x) for x in possible_long]
+        self.possible = [] # Why don't we just do this with another set?
+                            # Running into trouble trying it. Gotta figure out why.
+        for x in possible_long:
+            if x not in self.possible:
+                self.possible.append(x)
         self.set_possible_ints()
+
 
     def find_subtraction_values(self):
         for x in range(self.formula[1] + 1, puzzle_size + 1):
@@ -109,7 +53,17 @@ n.b.b.: After running through this by hand, it missed 3 4 5 5. Seemed to capture
             self.possible.append([x, x * self.formula[1]])
         self.set_possible_ints()
 
+    def check_multiplication_value(self, arr):
+        check = 1
+        for x in arr:
+            check *= x
+        return check
 
-def sum_test(cluster_size, cluster_sum, puzzle_size):
-    test = [1] * cluster_size
-    for i, x in enumerate(reversed(test)):
+    def find_multiplication_values(self):
+        arr = []
+        for x in range(1, puzzle_size + 1):
+            if self.formula[1] % x == 0:
+                arr += [x] * self.cluster_size
+            possible_long = list(set(x for x in itertools.combinations(
+                arr, self.cluster_size) if self.check_multiplication_value(x)
+                == self.formula[1]))
